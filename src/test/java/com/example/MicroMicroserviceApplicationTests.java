1c719f78c0e5f8d80044bdf4122188fd5e5f02af
Josh Long <josh@joshlong.com>
2016-05-03 16:08:46 -0700

    YOLO

---
+++ src/test/java/com/example/MicroMicroserviceApplicationTests.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MicroMicroserviceApplication.class)
+@WebAppConfiguration
+public class MicroMicroserviceApplicationTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
+
+	@Before
+	public void before() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+				.build();
+	}
+
+	@Test
+	public void testHello() throws Exception {
+
+		this.mockMvc.perform(get("/hello").accept(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(mvcResult -> Assert.assertTrue(
+						mvcResult.getResponse().getContentAsString().contains("Hello, world!")));
+
+
+	}
+
+}
